da0bfe00d7e7063ba29601a010199ea5759fdbbb
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2013-09-02 00:30:58 +0200

    msg

---
+++ app.js
@@ -0,0 +1,261 @@
+var http = require('http');
+var url = require("url");
+var request = require('request');
+var cache = require('memory-cache');
+var pme2ical = require('./pme2ical');
+var azure = require('azure');
+var crypto = require('crypto');
+var zlib = require('zlib');
+
+//configuration vars
+var urlPME = process.env.PME_URL_WEBACCESS;
+var urlGetData = process.env.PME_URL_GETDATA;
+var lookDaysBack = 4 * 7;
+var lookDaysAhead = 13 * 7;
+var sResourceHuman = process.env.PME_RESOURCE_ALLHUMAN || '45056';
+var sResourceToPlan = process.env.PME_RESOURCE_PERSONAL || '53248';
+var cacheTimeout = (process.env.PME2ICAL_CACHETIMEOUTMINUTES || 15) * 60 * 1000; //cache PME-results for 15 minutes
+var cyphersecret = process.env.PME2ICAL_CYPHERSECRET;
+var authPMEUser = {
+	'user': process.env.PME_USERNAME,
+	'pass': process.env.PME_PASSWORD,
+	'sendImmediately': true
+};
+var blobAccount = process.env.AZURE_STORAGE_ACCOUNT;
+var blobKey = process.env.AZURE_STORAGE_ACCESS_KEY;
+
+
+http.createServer(function (httpRequest, httpResponse) {
+
+    console.log("==> Requested url: " + httpRequest.url);
+    
+    //force https (only in production mode)
+    if (!(httpRequest.headers['x-forwarded-proto'] === 'https' || httpRequest.headers['x-arr-ssl'] || process.env.NODE_ENV === 'development')) {
+        httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+        httpResponse.end("Only http requests allowed.");
+        return;
+    }
+
+    if (httpRequest.url.toLowerCase().indexOf('/planningpme.ics?token=') === 0) {
+
+        var token = url.parse(httpRequest.url, true).query['token'];
+        var mode = url.parse(httpRequest.url, true).query['mode'] || 'event'; //event (full-day) or appointment
+
+        var decipher = crypto.createDecipher('aes256', cyphersecret);
+        var entityId = parseInt(decipher.update(token, 'hex', 'utf8') + decipher.final('utf8'), 10);
+        console.log('Deciphered entityId = ' + entityId);
+
+        getPmeResults(function (err, pmeResults) {
+            if (err) {
+                httpResponse.writeHead(200, { 'Content-Type': 'text/html' }); //internal server error?
+                httpResponse.end("Problem getting PME data from Azure Blob Storage. Please call Intern Support.");
+            }
+            else {
+                ReturnIcalFeed(pmeResults, entityId, mode, httpResponse);
+            }
+        });
+    }
+    else if (httpRequest.url.toLowerCase().indexOf('/gettokenfor/') === 0) {
+        var name = httpRequest.url.toLowerCase().split('/').slice(-1)[0];
+        var today = new Date().setHours(0, 0, 0, 0);
+
+        getPmeResults(function (error, pmeResults) {
+            var found = false;
+            if (!error) {
+                var yEntities = pmeResults['value'].YEntities;
+                for (var i = 0; i < yEntities.length; i++) {
+                    yEntity = yEntities[i];
+                    if (yEntity.N.replace(/ /g, "").toLowerCase() === name) {
+                        var cipher = crypto.createCipher('aes256', cyphersecret);
+                        var encrypted = cipher.update(yEntity.Id.toString(), 'utf8', 'hex') + cipher.final('hex');
+                        httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+                        httpResponse.end("Your personal token was generated. Use this url in Outlook as new Internet Calendar Subscription -> webcal://" + httpRequest.headers.host + "/PlanningPME.ics?token=" + encrypted);
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+                httpResponse.end("Resource '" + name + "' not found in PlanningPME.");
+            }
+        });
+    }
+    else if (httpRequest.url.toLowerCase() === '/gettoken') {
+
+        var auth = httpRequest.headers['authorization'];
+        if (!auth) {
+            return401(httpResponse);
+        }
+
+        if (auth) {
+            var buf = new Buffer(auth.split(' ')[1], 'base64');
+            var creds = buf.toString().split(':');
+            var username = creds[0];
+            var password = creds[1];
+
+            var today = new Date().setHours(0, 0, 0, 0);
+            var auth = {
+                'user': username,
+                'pass': password,
+                'sendImmediately': true
+            }
+            refreshPMEData(auth, today, today, sResourceToPlan, function (error, pmeResults) {
+                if (error) {
+                    if (error == "401") return401(httpResponse);
+                    if (error != "401") {
+                        httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+                        httpResponse.end("Something went wrong. Error: " + error);
+                    }
+                    return;
+                }
+
+                //console.log('pmeResults=' + pmeResults);
+                if (pmeResults.value && pmeResults.value.YEntities && pmeResults.value.YEntities.length === 1) {
+                    var cipher = crypto.createCipher('aes256', cyphersecret);
+                    var encrypted = cipher.update(pmeResults.value.YEntities[0].Id.toString(), 'utf8', 'hex') + cipher.final('hex');
+                    httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+                    httpResponse.end("Your personal token was generated. Use this url in Outlook as new Internet Calendar Subscription -> webcal://" + httpRequest.headers.host + "/PlanningPME.ics?token=" + encrypted);
+                }
+                else {
+                    httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+                    httpResponse.end("Error: could not find a resource in PlanningPME connected to your login. Ask ProjectBureau to add you as default resource in PlanningPME.");
+                }
+            });
+        }
+    }
+    else if (httpRequest.url.toLowerCase() === '/cc') {
+        cache.clear();
+        httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+        httpResponse.end("Cache is cleared...");
+    }
+    else if (httpRequest.url.toLowerCase() === '/refresh') {
+        var today = new Date().setHours(0, 0, 0, 0);
+        var startDate_ms = today - (1000 * 60 * 60 * 24 * lookDaysBack);
+        var endDate_ms = today + (1000 * 60 * 60 * 24 * lookDaysAhead);
+
+        refreshPMEData(authPMEUser, startDate_ms, endDate_ms, sResourceHuman, function (error, pmeResults) {
+            if (error || !pmeResults) return;
+
+            cache.put('pmeResults', pmeResults, cacheTimeout); //put in cache
+            var pmeResultsString = JSON.stringify(pmeResults);
+
+            zlib.deflate(pmeResultsString, function (err, buffer) {
+                if (!err) {
+                    var blobService = azure.createBlobService();
+                    blobService.createBlockBlobFromText('data', 'pme.json.zip.b64', buffer.toString('base64'), function (err, results) { });
+
+                }
+            });
+        });
+        httpResponse.writeHead(200, { 'Content-Type': 'text/html' });
+        httpResponse.end("PME data is being refreshed...");
+    }
+    else {
+        httpResponse.writeHead(400, { 'Content-Type': 'text/html' });
+        httpResponse.end("Wrong request.");
+    }
+
+}).listen(process.env.PORT || 8080);
+
+function getPmeResults(callback) {
+    //first try cache
+    var pmeResults = cache.get('pmeResults');
+    if (pmeResults) return callback(null, pmeResults);
+
+    //then blob storage
+    azure.createBlobService().getBlobToText('data', 'pme.json.zip.b64', function (err, results) {
+        if (err) return callback(err, null);
+
+        //unzip pme data
+        var buffer = new Buffer(results, 'base64');
+        zlib.unzip(buffer, function (err, buffer) {
+            if (err) return callback(err, null);
+            pmeResults = JSON.parse(buffer.toString());
+            if (pmeResults) cache.put('pmeResults', pmeResults, cacheTimeout); //put back in cache
+            callback(null, pmeResults);
+        });
+    });
+}
+
+function return401(httpResponse)
+{
+    // No Authorization header was passed in so it's the first time the browser hit us
+    // Sending a 401 will require authentication, we need to send the 'WWW-Authenticate' to tell them the sort of authentication to use
+    // Basic auth is quite literally the easiest and least secure, it simply gives back  base64( username + ":" + password ) from the browser
+    httpResponse.writeHead(401, { 'WWW-Authenticate': 'Basic realm="MACAW"' });
+    httpResponse.end('<html><body>Need some creds son</body></html>');
+}
+
+function ReturnIcalFeed(pmeResults, entityId, mode, httpResponse) {
+
+    var ical = pme2ical.getICalendarFeed(pmeResults, entityId, mode);
+    httpResponse.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    httpResponse.end(ical);
+}
+
+function refreshPMEData(auth, startDate_ms, endDate_ms, sResource, callback) {
+
+    console.time("StartSession");
+    var j = request.jar(); //new cookie jar for each PME sessions
+    request(urlPME, {
+        'auth': auth,
+        'jar': j
+    }, function (error, response, body) {
+        console.timeEnd("StartSession");
+        if (checkError(error, response, callback)) return;
+
+        //get timezone offset
+        console.time("GetTimezoneOffset");
+        request.post(urlGetData + "?ts=" + new Date().getTime(), {
+            'headers': { "X-AjaxPro-Method": "GetParameter" },
+            'json': true,
+            'jar': j,
+            'body': '{ "dt": "/Date(' + new Date().setHours(0, 0, 0, 0) + ')/" }'
+        }, function (error, response, body) {
+            console.timeEnd("GetTimezoneOffset");
+            if (checkError(error, response, callback)) return;
+
+            var tzo = -body['value'].TZO;
+
+            var getDataParams = {
+                "yview": 1,
+                "sdate": "/Date(" + startDate_ms + ")/",
+                "edate": "/Date(" + endDate_ms + ")/",
+                "sResource": sResource,
+                "sSkill": "0",
+                "sState": "-1",
+                "sCustomer": "0",
+                "sProject": "0",
+                "sSubProject": null,
+                "sTask": "NULL",
+                "sUnav": "NULL"
+            };
+
+            console.time("GetDatas");
+            request.post(urlGetData, {
+                'headers': { "X-AjaxPro-Method": "GetDatas" },
+                'json': true,
+                'jar': j,
+                'body': getDataParams
+            }, function (error, response, pmeResults) {
+                console.timeEnd("GetDatas");
+                if (checkError(error, response, callback)) return;
+                console.log('==> pmeResults loaded');
+
+                pmeResults.TZO = tzo;
+
+                callback(null, pmeResults);
+            }); //.pipe(fs.createWriteStream('pme.json'));
+        });
+    });
+}
+
+function checkError(error, response, callback) {
+    if (error || response.statusCode !== 200) {
+        console.log("[ERROR] Request error:" + (error || response.statusCode));
+        callback(error || response.statusCode, null);
+        return true;
+    }
+    return false; //no errors
+}
---
+++ pme2ical.js
@@ -0,0 +1,83 @@
+var icalendar = require('icalendar'); 
+
+//Information on icalendar specs:
+//  http://en.wikipedia.org/wiki/ICalendar
+//  http://www.ietf.org/rfc/rfc2445.txt
+//  http://msdn.microsoft.com/en-us/library/gg672059(v=exchg.80).aspx
+//  http://www.kanzaki.com/docs/ical/
+
+// a and b are javascript Date objects
+function dateDiffInHours(date1, date2) {
+  return Math.floor(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 60 * 30)) / 2;
+}
+
+function vEvent_setFullDay(vEvent, fullDay) {
+    vEvent.addProperty('DTSTART;VALUE=DATE', fullDay.getUTCFullYear() + ('0' + (fullDay.getUTCMonth()+1)).slice(-2) + ('0' + fullDay.getUTCDate()).slice(-2));
+    var eod = new Date(fullDay.getFullYear(), fullDay.getMonth(), fullDay.getDate()+1)
+    vEvent.addProperty('DTEND;VALUE=DATE', eod.getUTCFullYear() + ('0' + (eod.getUTCMonth()+1)).slice(-2) + ('0' + eod.getUTCDate()).slice(-2));
+}
+
+function getICalendarFeed(pmeResults, yEntityId, mode) {
+	if (!pmeResults || !yEntityId || !pmeResults['value'] || !pmeResults['TZO']) return;
+
+	var tzOffset = pmeResults['TZO']; //timezone offset (Netherlands)
+	var yEntity, task, vEvent, startDateTime, endDateTime, startDateTimeCET, endDateTimeCET, startDateString, subject, eventDuration;
+
+    var ical = new icalendar.iCalendar();
+    ical.addProperty("X-PUBLISHED-TTL", "PT30M"); //PT30M = 30 minutes, PT1H = 1 hour
+    ical.addProperty("X-WR-CALNAME", "PlanningPME");
+
+    var tasks = pmeResults['value'].Tasks;
+	for (var i=0; i<tasks.length; i++) {
+		task = tasks[i];
+		if (task.RI === yEntityId) {
+            //get event times
+		    startDateTime = new Date(parseInt(task.SD.substr(6)) + tzOffset);
+            endDateTime = new Date(parseInt(task.ED.substr(6)) + tzOffset);
+		    startDateString = startDateTime.getUTCFullYear() + ('0' + (startDateTime.getUTCMonth()+1)).slice(-2) + ('0' + startDateTime.getUTCDate()).slice(-2);
+		    
+            //set id
+            vEvent = new icalendar.VEvent('PME-' + task.Id + '-' + startDateString); //add start-date because task's Id isn't always unique
+		    
+            //set start en end datetime
+            if (mode === 'appointment') {
+                vEvent.setDate(startDateTime, endDateTime);
+            }
+            else {
+                //mode event -> full day events
+                vEvent_setFullDay(vEvent, startDateTime);
+            }
+
+            //set subject
+		    subject = task.D;
+		    if (mode === 'event') {
+		        eventDuration = dateDiffInHours(startDateTime, endDateTime);
+		        if (eventDuration < 8) {
+		            startDateTimeCET = new Date(startDateTime.getTime() - tzOffset);
+		            endDateTimeCET = new Date(endDateTime.getTime() - tzOffset);
+		            subject = "[" + ('0' + startDateTimeCET.getUTCHours()).slice(-2) + ":" + ('0' + startDateTimeCET.getUTCMinutes()).slice(-2) +
+                    "-" + ('0' + endDateTimeCET.getUTCHours()).slice(-2) + ":" + ('0' + endDateTimeCET.getUTCMinutes()).slice(-2) + "] " + subject;
+		        }
+		    }
+            vEvent.setSummary(subject);
+
+            //set description
+            if (task.R) vEvent.setDescription(task.R.replace(/\r/g,""));
+
+		    if (mode === 'appointment') {
+                if ([7].indexOf(task.S) != -1) vEvent.addProperty("X-MICROSOFT-CDO-BUSYSTATUS", "FREE");
+                if ([3,4].indexOf(task.S) != -1) vEvent.addProperty("X-MICROSOFT-CDO-BUSYSTATUS", "TENTATIVE");
+                if ([0].indexOf(task.S) != -1) vEvent.addProperty("X-MICROSOFT-CDO-BUSYSTATUS", "BUSY");
+                if ([1,2,5,6,8].indexOf(task.S) != -1) vEvent.addProperty("X-MICROSOFT-CDO-BUSYSTATUS", "OOF");   
+		    }
+            else {
+                //fullday events are not visible in availibity searches
+                vEvent.addProperty("TRANSP", "TRANSPARENT"); //BUSYSTATUS = FREE 
+            }
+
+		    ical.addComponent(vEvent);
+		}
+	}
+	return ical.toString();
+}
+exports.getICalendarFeed = getICalendarFeed;
